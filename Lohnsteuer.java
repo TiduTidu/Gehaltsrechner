c2b1494c1590e642c66c5d869e7ceb6da003c2e4
DarijanAvric <avricdarijan@gmail.com>
2023-11-08 12:38:30 +0100

    LohnsteuerTabelle Klasse gebaut und setLohnSteuer Methode in User Klasse. Scanner in Lohnsteuer Klasse hinzufügt

--- Lohnsteuer.java
+++ Lohnsteuer.java
@@ -1,15 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Lohnsteuer {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Scanner scanner = new Scanner(System.in);
+
+        //#region Bruttolohn und Steuerklasse eingabe
         System.out.println("Lohnsteur rechner | Monatsgehalt");
         System.out.println("------------------------------");
 
-        System.out.println("Bitte geben Sie ihren Lohn ein");
+        System.out.println("Bitte geben Sie ihren Lohn ein: ");
+        int bruttoLohnEingabe = scanner.nextInt(); 
 
-        String lohn = reader.readLine();
+        System.out.println("Bitte geben Sie ihren Steuerklasse ein: (1-6)");
+        int steuerKlasseEingabe = scanner.nextInt();
+        scanner.close();
+
+        User user = new User(bruttoLohnEingabe, steuerKlasseEingabe, 29, false, "AOK", "NRW", 0);
+    
+        User.setLohnSteuer(user);
+    
+        System.out.println(user.lohnSteuer);
+        
+    
     }
 }
--- User.java
+++ User.java
@@ -6,11 +6,10 @@ public class User {
     boolean kirchenSteuer;
     String versicherung;
     String bundesland;
-    
-
+    int lohnSteuer;
 
     //Konstruktor für neuen User/Steuerzahler
-    public User(int bruttoLohn, int steuerKlasse, int alter, boolean kirchenSteuer, String versicherung, String bundesland)
+    public User(int bruttoLohn, int steuerKlasse, int alter, boolean kirchenSteuer, String versicherung, String bundesland, int lohnSteuer)
     {
         this.bruttoLohn = bruttoLohn;
         this.steuerKlasse = steuerKlasse;
@@ -18,5 +17,24 @@ public class User {
         this.kirchenSteuer = kirchenSteuer;
         this.versicherung = versicherung;
         this.bundesland = bundesland;
+        this.lohnSteuer = lohnSteuer;
+
     }
+
+    public static void setLohnSteuer(User user) {
+        int[][] lohnsteuerTabelle = LohnsteuerTabelle.getLohnsteuerTabelle();
+
+        for(int i = 0; i < lohnsteuerTabelle.length; i++) {
+            int untereGrenze = lohnsteuerTabelle[i][0];
+            int obereGrenze = (i < lohnsteuerTabelle.length - 1) ? lohnsteuerTabelle[i + 1][0] : Integer.MAX_VALUE;
+
+            if(user.bruttoLohn >= untereGrenze && user.bruttoLohn <= obereGrenze) {
+                if(user.steuerKlasse >= 1 && user.steuerKlasse <= 6) {
+                    user.lohnSteuer = lohnsteuerTabelle[i][user.steuerKlasse];
+                }
+            }
+
+        }
+    }
+
 }
