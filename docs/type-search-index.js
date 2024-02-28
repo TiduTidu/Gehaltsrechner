bb94b92cf0a644744c93d72295cec2f208b2436e
yussuf.sassi <yussuf.sassi@krankikom.de>
2024-02-28 16:19:52 +0100

    Code cleanup and add docs

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"Alle Klassen und Schnittstellen","u":"allclasses-index.html"},{"p":"com.c3c2","l":"LohnsteuerTabelle"},{"p":"com.c3c2","l":"Main"},{"p":"com.c3c2","l":"PdfWriter"},{"p":"com.c3c2","l":"User"},{"p":"com.c3c2","l":"Utils"},{"p":"com.c3c2","l":"Validator"}];updateSearchResults();
